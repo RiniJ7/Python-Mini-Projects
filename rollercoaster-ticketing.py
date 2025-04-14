1a0c0ba24c07b82aa7dc58531c35e5ad1c94593b
rinij <rinimani7@gmail.com>
2025-04-14 13:49:38 -0600

    logical operators added to define age category

--- rollercoaster-ticketing.py
+++ rollercoaster-ticketing.py
@@ -2,25 +2,28 @@
 
 
 height = int(input("Please enter your height?"))
-if(height > 120):
+if(height >= 120):
     print("You are eligible for this ride")
     print("Adult tickets are $12 per head")
     print("Youth tickets are $7 per head")
     print("Kids tickets are $5 per head")
-    category = input("Please enter the ticket category(Adult/Youth/Kids)")
-    if(category == "Adult"):
-        bill = 12
-        print(" Your bill is $12")
-    elif(category == "Youth"):
-        bill = 7
-        print(" Your bill is $7")
-    elif(category == "Kids"):
+    age = int(input("What is your age?"))
+    if age < 12 :
         bill = 5
-        print(" Your bill is $5")
+        print(f" Your bill is ${bill}")
+    elif age < 18:
+        bill = 7
+        print(f" Your bill is ${bill}")
+    elif age >= 45 and age <= 55:
+        bill = 0
+        print("Everything is going to be okay and have a free ride on us!")
+    else:
+        bill = 12
+        print(f" Your bill is ${bill}")
     wants_photo = input ("Do you want to have a photo taken? Type y for yes and n for No")
     if wants_photo == 'y':
         bill += 3
-    print (f"Your bill is ${bill}")
+        print(f" Your bill is ${bill}")
 else:
     print("Sorry, you do not meet the requirements for this ride")    
                    
