bf81a6229f8c801c767a4d45ea7402363d0a75b7
rinij <rinimani7@gmail.com>
2025-05-13 12:13:28 -0600

    extracting key and values from the dictionary

--- dictionary.py
+++ dictionary.py
@@ -9,3 +9,10 @@ print(programming_dictionary["Function"])
 #adding something to a dictionary
 programming_dictionary["loop"] = "Something which iterates"
 print(programming_dictionary)
+
+
+for thing in programming_dictionary:
+    #for printing key
+    print(thing)
+    #for printing value
+    print(programming_dictionary[thing])
