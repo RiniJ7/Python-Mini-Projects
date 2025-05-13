42f8680ee36936c482071380b9db4883422ea82c
rinij <rinimani7@gmail.com>
2025-05-13 11:55:27 -0600

    python dictionaries coded

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+programming_dictionary = {
+    "Bug": "This is an error",
+    "Function":"This is a function"
+}
+
+#extracting a key from a dictionary
+print(programming_dictionary["Function"])
+
+#adding something to a dictionary
+programming_dictionary["loop"] = "Something which iterates"
+print(programming_dictionary)
